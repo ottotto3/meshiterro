2932db795909fe8456b6bb70bb97da849fe552de
EC2 Default User <ec2-user@ip-172-31-7-37.ap-northeast-1.compute.internal>
2024-02-20 07:17:31 +0000

    [Add]18章

--- test/controllers/post_images_controller_test.rb
+++ test/controllers/post_images_controller_test.rb
@@ -1,18 +1,7 @@
 require "test_helper"
 
 class PostImagesControllerTest < ActionDispatch::IntegrationTest
-  test "should get new" do
-    get post_images_new_url
-    assert_response :success
-  end
-
-  test "should get index" do
-    get post_images_index_url
-    assert_response :success
-  end
-
-  test "should get show" do
-    get post_images_show_url
-    assert_response :success
-  end
+  # test "the truth" do
+  #   assert true
+  # end
 end
